d95d3b38f4344653803bdd136b2d4c6126f652e6
Masumits <tanya.masumi@mail.ru>
2016-12-29 13:22:25 +0300

    Delete table.sql

--- exam29/table.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE my_table (id INTERGER PRIMARY KEY, wordform VARCHAR(100), lemma VARCHAR(100), pos VARCHAR(100) ;
-INSERT INTO my_table (id, wordform, lemma, pos) VALUES (0, современных, современный, A);
